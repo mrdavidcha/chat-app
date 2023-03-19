b72bd0fb076ca82bbcd1c611cdfc202fcaee8dbd
🤷 <dave_spec@yahoo.com>
2023-03-18 20:44:33 -0500

    nothing much

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,15 +1,13 @@
 export default function Index() {
   return (
     <>
-      <div className="font-sans leading-5 m-4">
-        <h1 className="text-1xl font-bold text-stone-600">Welcome to Remix</h1>
-        <h2 className="text-2xl font-bold text-stone-600">Welcome to Remix</h2>
-        <h3 className="text-3xl font-bold text-stone-600">Welcome to Remix</h3>
-        <h4 className="text-4xl font-bold text-stone-600">Welcome to Remix</h4>
-        <h5 className="text-5xl font-bold text-stone-600">Welcome to Remix</h5>
-        <h1 className="text-6xl font-bold text-stone-600">Welcome to Remix</h1>
-        <h1 className="text-7xl font-bold text-stone-600">Welcome to Remix</h1>
-        <h1 className="text-8xl font-bold text-stone-600">Welcome to Remix</h1>
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-wrap items-center">
+        <h1 className="text-7xl font-bold text-stone-600">Ask me anything:</h1>
+        </div>
+        <div className="flex flex-wrap items-center">
+          <input type="text" id="Question" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type question here" required/>
+        </div>
       </div>
     </>
   );
