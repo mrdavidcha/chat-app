57e9d4de848e32560d8212215097e48e3011b025
🤷 <dave_spec@yahoo.com>
2023-03-18 19:42:28 -0500

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,16 @@
+export default function Index() {
+  return (
+    <>
+      <div className="font-sans leading-5 m-4">
+        <h1 className="text-1xl font-bold text-stone-600">Welcome to Remix</h1>
+        <h2 className="text-2xl font-bold text-stone-600">Welcome to Remix</h2>
+        <h3 className="text-3xl font-bold text-stone-600">Welcome to Remix</h3>
+        <h4 className="text-4xl font-bold text-stone-600">Welcome to Remix</h4>
+        <h5 className="text-5xl font-bold text-stone-600">Welcome to Remix</h5>
+        <h1 className="text-6xl font-bold text-stone-600">Welcome to Remix</h1>
+        <h1 className="text-7xl font-bold text-stone-600">Welcome to Remix</h1>
+        <h1 className="text-8xl font-bold text-stone-600">Welcome to Remix</h1>
+      </div>
+    </>
+  );
+}
