fe1f35555760cec3ab489b74c4d329e80e152e9f
🤷 <dave_spec@yahoo.com>
2023-03-19 10:43:40 -0500

    working with api now, needs more styling

---
+++ app/models/chat.server.ts
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const openai = new OpenAIApi(new Configuration({
+  apiKey: process.env.API_KEY
+}));
+
+export async function getOpenAI(question: string) {
+
+  console.log('question lol', question);
+  const res = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [{
+      role: 'user',
+      content: question
+    }]
+  }).then((res) => res?.data?.choices[0]?.message?.content);
+  return res;
+
+  // return res.data.choices;
+
+  // const res = await fetch(
+  //   'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
+  // ).then((res) => res.json());
+
+  // return res.results;
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,50 @@
+import type { ActionArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useActionData, Form } from "@remix-run/react";
+import { getOpenAI } from "../models/chat.server";
+
+type ChatGPTData = {
+  res: Awaited<ReturnType<typeof getOpenAI>>;
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData();
+  const question:any = formData.get('question');
+  console.log('question ---> ', question);
+
+  const res = await getOpenAI(question);
+
+  return json<ChatGPTData>({ res });
+}
+
 export default function Index() {
+  const data = useActionData();
+
+  console.log('res ????', data?.res);
+
   return (
-    <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="flex flex-wrap items-center">
-        <h1 className="text-7xl font-bold text-stone-600">Ask me anything:</h1>
+    <div className="sm">
+      <div className="max-w-screen-md flex flex-wrap flex-col p-6 place-content-end">
+        <div className="grow">
+          <h1 className="text-6xl font-bold text-stone-600">Ask me anything:</h1>
         </div>
-        <div className="flex flex-wrap items-center">
-          <input type="text" id="Question" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type question here" required/>
+        <div className="grow pt-3">
+          <Form method="post">
+            <input
+              type="text"
+              autoComplete={"false"}
+              id="question"
+              name="question"
+              className="border text-gray-500 text-lg focus:ring-blue-500 focus:border-gray-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="What would you like to know?"
+              required
+            />
+            <button type="submit"  className="border p-1.5 text-lg text-gray-500 mt-3 hover:text-gray-800">Submit</button>
+          </Form>
+
+          <span>{data?.res}</span>
         </div>
       </div>
-    </>
+    </div>
   );
 }
